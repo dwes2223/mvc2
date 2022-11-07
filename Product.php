b174434baaa6889349171f3c20b9ef5407e4acda
Rafa Cabeza <rafacabeza@gmail.com>
2022-11-07 16:31:01 +0100

    mvc00

---
+++ Product.php
@@ -0,0 +1,26 @@
+<?php
+   // Fichero que simula el modelo con datos
+  class Product{
+    const PRODUCTS = [
+        array(1,'Cortacesped'),
+        array(2,'Pizarra'),
+        array(3,'Billar'),
+        array(4,'Dardos'),
+    ];
+
+function __construct()  { /*cons vacio*/   }
+
+//devuelve todos los productos
+public static function all()
+{
+    return Product::PRODUCTS;
+}
+
+//devolver un producto en particular
+public function find($id)
+{
+    return Product::PRODUCTS[$id-1];
+}
+
+
+}//fin_CLASE
